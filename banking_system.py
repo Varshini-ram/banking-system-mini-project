4524986081b94290a081ace33a1b408cf78d4b41
Varshini-ram <169165137+Varshini-ram@users.noreply.github.com>
2026-09-21 19:47:21 +0530

    Added main menu

--- banking_system.py
+++ banking_system.py
@@ -39,3 +39,25 @@ def login():
         print("Account not found.")
 
     return None
+
+def main():
+    while True:
+        print("\n===== SIMPLE SMART BANKING SYSTEM =====")
+        print("1. Create Account")
+        print("2. Login")
+        print("3. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            create_account()
+        elif choice == "2":
+            login()
+        elif choice == "3":
+            print("Thank you for using our banking system!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+main()
