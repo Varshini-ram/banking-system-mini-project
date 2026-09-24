42d5be7abf211a0564ac4b6c8478e93028e32dfc
Varshini-ram <169165137+Varshini-ram@users.noreply.github.com>
2026-09-24 22:49:17 +0530

    Added deposit feature

--- banking_system.py
+++ banking_system.py
@@ -44,6 +44,15 @@ def check_balance(account_number):
     balance = accounts[account_number]["balance"]
     print("\n--- ACCOUNT BALANCE ---")
     print("Current Balance: ₹", balance)
+def deposit(account_number):
+    amount = float(input("Enter amount to deposit: ₹ "))
+
+    if amount > 0:
+        accounts[account_number]["balance"] += amount
+        print("Deposit successful!")
+        print("New Balance: ₹", accounts[account_number]["balance"])
+    else:
+        print("Please enter a valid amount.")
 def account_menu(account_number):
     while True:
         print("\n===== ACCOUNT MENU =====")
@@ -59,6 +68,8 @@ def account_menu(account_number):
 
         if choice == "1":
             check_balance(account_number)
+        elif choice == "2":
+            deposit(account_number)
         elif choice == "7":
             print("Logged out successfully.")
             break
